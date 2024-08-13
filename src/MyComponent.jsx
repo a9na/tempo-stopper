1e38286b33bc1aa56c623ed6b3175425094449e3
a9na <anaiscoding@gmail.com>
2024-08-13 22:48:20 +0200

    commit

---
+++ src/MyComponent.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+
+function MyComponent(){
+
+   
+
+    return(
+    <div>
+        
+    </div> 
+);
+}
+
+
+export default MyComponent;
