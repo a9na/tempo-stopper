ca68e26ce7f170e2e7d68917049dc7862b2a9dfd
a9na <anaiscoding@gmail.com>
2024-08-14 11:56:49 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import MyComponent from './MyComponent.jsx';
+import Stopwatch from './Stopwatch.jsx';
 
 
 function App() {
   
 
-  return(<MyComponent/> 
+  return(<Stopwatch /> 
   );
 
 }
